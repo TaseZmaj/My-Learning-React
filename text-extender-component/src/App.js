e27d793a069637c2b8b27d489bbb535a8da2d60a
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2024-12-17 01:31:03 +0100

    adding an already finished project in order to analyze its code

--- text-extender-component/src/App.js
+++ text-extender-component/src/App.js
@@ -93,3 +93,36 @@ function TextExpander({
 }
 
 // Version 2
+// function TextExpander({
+//   collapsedNumWords = 10,
+//   expandButtonText = "Show more",
+//   collapseButtonText = "Show less",
+//   buttonColor = "#1f09cd",
+//   expanded,
+//   className,
+//   children,
+// }) {
+//   const [isExpanded, setIsExpanded] = useState(expanded);
+
+//   const displayText = isExpanded
+//     ? children
+//     : children.split(" ").slice(0, collapsedNumWords).join(" ") + "...";
+
+//   const buttonStyle = {
+//     background: "none",
+//     border: "none",
+//     font: "inherit",
+//     cursor: "pointer",
+//     marginLeft: "6px",
+//     color: buttonColor,
+//   };
+
+//   return (
+//     <div className={className}>
+//       <span>{displayText}</span>
+//       <button style={buttonStyle} onClick={() => setIsExpanded((exp) => !exp)}>
+//         {isExpanded ? collapseButtonText : expandButtonText}
+//       </button>
+//     </div>
+//   );
+// }
