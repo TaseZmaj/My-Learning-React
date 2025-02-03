c6dabffe3f8744c1dbc1b74c5bbae5c6ae72afb7
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2025-02-03 22:34:30 +0100

    deleted a file

---
+++ currency-converter-moe/src/App.js
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div>
+      <input type="text" />
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">USD</option>
+        <option value="CAD">USD</option>
+        <option value="INR">USD</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">USD</option>
+        <option value="CAD">USD</option>
+        <option value="INR">USD</option>
+      </select>
+      <p>OUTPUT</p>
+    </div>
+  );
+}
+
+export default App;
