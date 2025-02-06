20d5252f8568740982f7a8c5f70bbf2daf923e1d
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2025-02-06 16:01:13 +0100

    practising some custom hooks, made a useGeolocation Hook

--- geolocation-hook-moe/src/App.js
+++ geolocation-hook-moe/src/App.js
@@ -1,5 +1,44 @@
-function App() {
-  return <p>DA</p>;
-}
+import { useState } from "react";
+import { useGeolocation } from "./useGeolocation";
 
-export default App;
+export default function App() {
+  const {
+    isLoading,
+    error,
+    getPosition,
+    position: { lat, lng }, //NESTED DESTRUCTURING
+  } = useGeolocation();
+
+  const [countClicks, setCountClicks] = useState(0);
+  //const {lat, lng} = position;
+
+  function handleClick() {
+    setCountClicks((c) => c + 1);
+    getPosition();
+  }
+
+  return (
+    <div>
+      <button onClick={() => handleClick()} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
+        <p>
+          Your GPS position:{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
+  );
+}
