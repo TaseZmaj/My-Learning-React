406d169e499c2dd5c323c2c269eff6d7d9748560
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2025-02-17 21:03:17 +0100

    exported the PostContext context provider in a separate file

---
+++ 12-atomic-blog-moe/src/PostContext.js
@@ -0,0 +1,60 @@
+import { createContext, useState } from "react";
+import { faker } from "@faker-js/faker";
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+// 1) CREATE A CONTEXT
+const PostContext = createContext();
+
+function PostProvider({ children }) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Derived state. These are the posts that will actually be displayed
+  //PAZI: pazi gi searchedPosts -> ako titleot ili body-to sodrzat searchQuery
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  return (
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+}
+
+//namesto Default export, ...
+// export default PostProvider;
+
+//napravi Named export:
+export { PostProvider, PostContext };
+//bidejki morasi PostProvider-ot i ContextProvider-ot da gi exportnesh
+//bidejki gi koristis na posebni mesta
