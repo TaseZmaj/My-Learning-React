e2c9893beb63a4cd766a8bbb915e757d6cf1d3b7
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2025-02-17 19:17:53 +0100

    Was analyzing the App, added some comments

--- 12-atomic-blog-moe/src/App.js
+++ 12-atomic-blog-moe/src/App.js
@@ -16,6 +16,7 @@ function App() {
   const [isFakeDark, setIsFakeDark] = useState(false);
 
   // Derived state. These are the posts that will actually be displayed
+  //PAZI: pazi gi searchedPosts -> ako titleot ili body-to sodrzat searchQuery
   const searchedPosts =
     searchQuery.length > 0
       ? posts.filter((post) =>
@@ -155,10 +156,15 @@ function List({ posts }) {
 }
 
 function Archive({ onAddPost }) {
-  // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
+  // Here we don't need the setter function. We're only using state to store these posts
+  // because the callback function passed into useState (which generates the posts) is
+  // only called once, on the initial render. So we use this trick as an optimization
+  // technique, because if we just used a regular variable, these posts would be re-created
+  //  on every render. We could also move the posts outside the components, but I wanted
+  // to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
-    Array.from({ length: 10000 }, () => createRandomPost())
+    Array.from({ length: 10 }, () => createRandomPost())
   );
 
   const [showArchive, setShowArchive] = useState(false);
