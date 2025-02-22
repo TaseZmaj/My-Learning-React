51f047c4aab3d0540c7d1697c95b40802cb6585e
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2025-02-22 23:50:51 +0100

    added a comment

--- 12-atomic-blog-moe/src/App-memo.js
+++ 12-atomic-blog-moe/src/App-memo.js
@@ -159,6 +159,7 @@ function List({ posts }) {
   );
 }
 
+//NE RABOTI memo() ako prop-ot vo komponentot e objekt
 const Archive = memo(function Archive({ archiveOptions }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
