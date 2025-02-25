03b78e46c5fcdea0a8cfc930faac182803d3922f
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2025-02-25 19:14:09 +0100

    learning some classic oldschool Redux practises

--- 15-redux-intro-moe/src/store.js
+++ 15-redux-intro-moe/src/store.js
@@ -1,10 +1,19 @@
-const initialState = {
+/* eslint-disable no-unused-vars */
+import { createStore } from "redux"; //depricated e createStore
+
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
       return {
@@ -19,7 +28,12 @@ function reducer(state = initialState, action) {
     case "account/requestLoan":
       if (state.loan > 0) return state;
       //LATER
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case "account/payLoan":
       return {
         ...state,
@@ -31,4 +45,45 @@ function reducer(state = initialState, action) {
       //vo Redux e preporacano namesto Error, da returnesh samo state
       return state;
   }
+}
+
+function customerReducer() {}
+//TODO: finish customerReducer
+
+const store = createStore(accountReducer);
+
+//Vo stari kodovi se koristi vaka, da ne se cudis
+// const ACCOUNT_DEPOSIT = "account/deposit";
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return { type: "account/requestLoan", payload: { amount, purpose } };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+// store.dispatch(deposit(500));
+// store.dispatch(withdraw(400));
+// store.dispatch(requestLoan(12000, "Buying a car"));
+// store.dispatch(payLoan(500));
+// console.log(store.getState());
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return {
+    type: "account/updateName",
+    payload: { fullName },
+  };
 }
