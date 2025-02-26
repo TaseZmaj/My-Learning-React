b99e3e642060747e1d5f76532bf60251f255656a
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2025-02-26 02:30:43 +0100

    Practiced some classic Reducer

--- 15-redux-intro-moe/src/features/account/AccountOperations.js
+++ 15-redux-intro-moe/src/features/account/AccountOperations.js
@@ -1,4 +1,9 @@
-import { useState } from "react";
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+import { deposit, withdraw, requestLoan, payLoan } from "./accountSlice";
 
 function AccountOperations() {
   const [depositAmount, setDepositAmount] = useState("");
@@ -7,13 +12,42 @@ function AccountOperations() {
   const [loanPurpose, setLoanPurpose] = useState("");
   const [currency, setCurrency] = useState("USD");
 
-  function handleDeposit() {}
+  const dispatch = useDispatch();
+  const {
+    loan: currentLoan,
+    loanPurpose: currentLoanPurpose,
+    balance,
+  } = useSelector((store) => store.account);
 
-  function handleWithdrawal() {}
+  // useEffect(() => {
+  //   console.log("Account: ", account);
+  // }, [account]);
 
-  function handleRequestLoan() {}
+  function handleDeposit() {
+    if (!depositAmount) return;
 
-  function handlePayLoan() {}
+    dispatch(deposit(Number(depositAmount)));
+    setDepositAmount("");
+  }
+
+  function handleWithdrawal() {
+    if (withdrawalAmount === 0) return;
+
+    dispatch(withdraw(Number(withdrawalAmount)));
+    setWithdrawalAmount("");
+  }
+
+  function handleRequestLoan() {
+    if (!loanAmount || !loanPurpose) return;
+
+    dispatch(requestLoan(Number(loanAmount), loanPurpose));
+    setLoanAmount("");
+    setLoanPurpose("");
+  }
+
+  function handlePayLoan() {
+    dispatch(payLoan());
+  }
 
   return (
     <div>
@@ -66,10 +100,12 @@ function AccountOperations() {
           <button onClick={handleRequestLoan}>Request loan</button>
         </div>
 
-        <div>
-          <span>Pay back $X</span>
-          <button onClick={handlePayLoan}>Pay loan</button>
-        </div>
+        {currentLoan > 0 && (
+          <div>
+            <span>Pay back {currentLoan} </span>
+            <button onClick={handlePayLoan}>Pay loan</button>
+          </div>
+        )}
       </div>
     </div>
   );
--- 15-redux-intro-moe/src/store.js
+++ 15-redux-intro-moe/src/store.js
@@ -1,89 +1,13 @@
 /* eslint-disable no-unused-vars */
-import { createStore } from "redux"; //depricated e createStore
+import { combineReducers, createStore } from "redux";
+import accountReducer from "./features/account/accountSlice.js";
+import customerReducer from "./features/customers/customerSlice.js";
 
-const initialStateAccount = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: "",
-};
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
 
-const initialStateCustomer = {
-  fullName: "",
-  nationalID: "",
-  createdAt: "",
-};
+const store = createStore(rootReducer);
 
-function accountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return {
-        ...state,
-        balance: state.balance + action.payload,
-      };
-    case "account/withdraw":
-      return {
-        ...state,
-        balance: state.balance - action.payload,
-      };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      //LATER
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-    default:
-      //vo Redux e preporacano namesto Error, da returnesh samo state
-      return state;
-  }
-}
-
-function customerReducer() {}
-//TODO: finish customerReducer
-
-const store = createStore(accountReducer);
-
-//Vo stari kodovi se koristi vaka, da ne se cudis
-// const ACCOUNT_DEPOSIT = "account/deposit";
-
-function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
-}
-function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-function requestLoan(amount, purpose) {
-  return { type: "account/requestLoan", payload: { amount, purpose } };
-}
-function payLoan() {
-  return { type: "account/payLoan" };
-}
-
-// store.dispatch(deposit(500));
-// store.dispatch(withdraw(400));
-// store.dispatch(requestLoan(12000, "Buying a car"));
-// store.dispatch(payLoan(500));
-// console.log(store.getState());
-
-function createCustomer(fullName, nationalID) {
-  return {
-    type: "customer/createCustomer",
-    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
-  };
-}
-
-function updateName(fullName) {
-  return {
-    type: "account/updateName",
-    payload: { fullName },
-  };
-}
+export default store;
